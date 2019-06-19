91781e1f94b876671230b057592beafbab864106
Yuichi Kubota <yuichikubota74@gmail.com>
2019-06-19 19:05:21 +0900

    QR code scanner.

--- app/src/main/java/com/example/cityguide/MapsActivity.kt
+++ app/src/main/java/com/example/cityguide/MapsActivity.kt
@@ -7,7 +7,10 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -17,28 +20,40 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
+import kotlinx.android.synthetic.main.activity_maps.*
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener, ActivityCompat.OnRequestPermissionsResultCallback {
 
+    /**
+     * Location
+     */
     private lateinit var map: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var lastLocation: Location
 
     private lateinit var locationCallback: LocationCallback
     private lateinit var locationRequest: LocationRequest
-    private var locationUpdateState = false
+    private var locationUpdateState: Boolean = false
+
+    /**
+     * QR code Scan
+     */
+    private var qrScanIntegrator: IntentIntegrator? = null
+
 
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
         private const val REQUEST_CHECK_SETTINGS = 2
+        private const val REQUEST_CAMERA_PERMISSION = 1
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
@@ -53,6 +68,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         }
 
         createLocationRequest()
+
+        qrScanIntegrator = IntentIntegrator(this)
+
+        rentButton.setOnClickListener{
+            scanQRCode()
+        }
 
     }
 
@@ -81,11 +102,22 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        Log.d("onActivityResult", "start")
         if (requestCode == REQUEST_CHECK_SETTINGS) {
             if (requestCode == Activity.RESULT_OK) {
                 locationUpdateState = true
                 startLocationUpdates()
             }
+        }
+
+        val result: IntentResult? = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents != null) {
+                Toast.makeText(this, result.contents, Toast.LENGTH_LONG).show()
+            }
+        } else {
+            Toast.makeText(this, "Fail!!", Toast.LENGTH_LONG).show()
+            super.onActivityResult(requestCode, resultCode, data)
         }
     }
 
@@ -99,6 +131,27 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
     override fun onResume() {
         super.onResume()
         if (!locationUpdateState) {
+            startLocationUpdates()
+        }
+    }
+
+
+    /**
+     * パーミッションリクエストの結果を受け取る
+     */
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.d("onRequestPermissionsResult", "start")
+        Log.d("onRequestPermissionsResult", requestCode.toString())
+//        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+//            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+////                requestCameraPermission()
+//            }
+//        } else {
+//            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        }
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            locationUpdateState = true
             startLocationUpdates()
         }
     }
@@ -133,7 +186,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         map.addMarker(markerOptions)
     }
 
+
     private fun startLocationUpdates() {
+        Log.d("startLocationUpdates", "start")
         if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
             return
@@ -169,4 +224,29 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
             }
         }
     }
+
+    private fun requestCameraPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.CAMERA)) {
+            Toast.makeText(this, "カメラのパーミッションが許可されていません", Toast.LENGTH_LONG).show()
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), 999)
+        }
+    }
+
+
+    /**
+     * Scan QR Code
+     */
+    private fun scanQRCode() {
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            qrScanIntegrator?.setPrompt("Scan a QR code")
+            qrScanIntegrator?.setBeepEnabled(false)
+            qrScanIntegrator?.setBarcodeImageEnabled(true)
+            qrScanIntegrator?.initiateScan()
+        } else {
+            requestCameraPermission()
+        }
+    }
+
 }
